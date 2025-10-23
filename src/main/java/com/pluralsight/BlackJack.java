9d05a4d14b75cf26aff066b65cc8350c08f4cee9
Alwin Zhao <alwinzhao12@gmail.com>
2025-10-23 15:57:48 -0400

    add askUserInt() static method and static scanner variable

--- src/main/java/com/pluralsight/BlackJack.java
+++ src/main/java/com/pluralsight/BlackJack.java
@@ -1,7 +1,17 @@
 package com.pluralsight;
 
+import java.util.Scanner;
+
 public class BlackJack {
+    Scanner myScanner = new Scanner(System.in);
+
     public static void main(String[] args) {
 
+    }
+    public int askUserInt(String question) {
+        System.out.println(question);
+        int output = myScanner.nextInt();
+        myScanner.nextLine();
+        return output;
     }
 }
